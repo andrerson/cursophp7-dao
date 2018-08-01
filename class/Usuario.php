6056a5ebb28e30093eaced726091496e1bce10b8
andrerson <andrersonn@gmail.com>
2018-08-01 01:18:30 -0300

    cramos a class usuario

--- class/Usuario.php
+++ class/Usuario.php
@@ -1,1 +1,74 @@
-class
+<?php 
+    class Usuario{
+
+        private $idusuario;
+        private $deslogin;
+        private $dessenha;
+        private $dtcadastro;
+
+        public function getIdusuario(){
+            return $this->idusuario;
+        }
+
+        public function getDeslogin(){
+            return $this->deslogin;
+        }
+        public function getDessenha(){
+            return $this->dessenha;
+        }
+        public function getDtcadastro(){
+            return $this->dtcadastro;
+        }
+
+        public function setIdusuario($idusuario){
+
+            $this->idusuario = $idusuario;
+        }
+
+        public function setDeslogin($deslogin){
+
+            $this->deslogin = $deslogin;
+        }
+
+        public function setDessenha($dessenha){
+
+            $this->dessenha = $dessenha;
+        }
+
+        public function setDtcadastro($dtcadastro){
+
+            $this->dtcadastro = $dtcadastro;
+        }
+
+
+        public function loadByid($id){
+
+            $sql = new Sql();
+
+            $result = $sql->select("SELECT * FROM tb_usuarios WHERE idusuario = :ID",array(
+                ":ID"=>$id
+            ));
+
+            if(isset($result[0])){
+                $row = $result[0];
+
+                $this->setIdusuario($row['idusuario']);
+                $this->setDeslogin(@$row['deslogin']);
+                $this->setDessenha($row['dessenha']);
+                $this->setDtcadastro(new DateTime(@$row['dtcadastro']));
+            }
+
+        }
+
+        public function __toString(){
+            return json_encode(array(
+                "idusuario"=>$this->getIdusuario(),
+                "deslogin"=>$this->getDeslogin(),
+                "dessenha"=>$this->getDessenha(),
+                "dtcadastro"=>$this->getDtcadastro()->format("d/m/Y H:i:s")
+            ));
+        }
+
+    }
+
+?>
--- index.php
+++ index.php
@@ -1,8 +1,15 @@
 <?php 
     require_once("config.php");
 
-    $sql = new Sql();
-    $usuairos = $sql->select("SELECT * FROM tb_usuarios ");
+    // $sql = new Sql();
+    // $usuairos = $sql->select("SELECT * FROM tb_usuarios ");
 
-    echo json_encode($usuairos); 
+    // echo json_encode($usuairos);
+    
+    
+    $root = new Usuario();
+
+    $root->loadByid(3);
+
+    echo $root;
 ?>
